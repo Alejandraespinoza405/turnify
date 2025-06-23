428f936c2d26fb884287b76d74b0be37ff5282cf
AlejandraEspinoza <rapuano.f@hotmail.com>
2025-06-23 16:49:06 -0300

    lifecicle

--- front/src/views/Home/MyAppointments/MyAppointments.jsx
+++ front/src/views/Home/MyAppointments/MyAppointments.jsx
@@ -1,18 +1,42 @@
 import { useState } from "react";
 import styles from "./MyAppointments.module.css";
-import { appointmentsData } from "../../../helpers/MyAppointment"
 import AppointmentCard from "../../../components/Navbar/AppointmentCard/AppointmentCard";
+import { useEffect } from "react";
+import axios from "axios";
 
 function MyAppointments() {
-    const [appointments, setAppointments] = useState(appointmentsData);
+    const [appointments, setAppointments] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const getAppointments = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get("http://localhost:3000/appointments");
+      setAppointments(response.data);
+      } catch (error) {
+        console.error(error);
+        setError("Ocurrió un error al solicitar sus turnos");
+      } finally {
+        setLoading(false);
+       }
+    };
+
+    useEffect(() => {
+      getAppointments();
+    }, []);
    
   return (
     <main className={styles.wrapper}>
       <h2 className={styles.title}>Turnos asignados</h2>
       <div className={styles.appointmentContainer}>
-        {appointments.map((appoint) => (
-          <AppointmentCard key={appoint.id} appointment={appoint} />
-        ))}
+        {loading ? ( 
+          <h3>Cargando...</h3>
+         ) : error ? ( 
+          <h3>{error}</h3> 
+          ) : (
+          appointments.map((appoint) => <AppointmentCard key={appoint.id} appointment={appoint} /> )
+          )}
       </div>
     </main>
   );
